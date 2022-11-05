025e4a7c10620cf3720bce4e52c3f6c6a5cdd516
mipunaruwan <nipunakalpa1234@gmail.com>
2022-11-05 16:55:54 +0530

    doctor  crud is completed

--- src/Controller/DoctorController.java
+++ src/Controller/DoctorController.java
@@ -1,15 +1,38 @@
 package Controller;
 
+import com.jfoenix.controls.JFXComboBox;
+import com.jfoenix.controls.JFXTextField;
 import db.DbConnection;
+import javafx.event.ActionEvent;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Alert;
+import javafx.scene.control.ButtonType;
+import javafx.scene.control.TextField;
+import model.Doctor;
 
 import javax.xml.transform.Result;
+import java.net.URL;
+import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.ResourceBundle;
 
-public class DoctorController {
+public class DoctorController implements Initializable {
+    public JFXTextField txtDid;
+    public JFXComboBox cmdEx;
+    public JFXTextField Dname;
+    public JFXTextField txtaddress;
+    public TextField DID;
+
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        cmdEx.getItems().addAll("1 year", "2 year", "3 year","4 year","5 year");
+    }
+
     public static List<String> getDoctorIds() throws SQLException, ClassNotFoundException {
         PreparedStatement stm = DbConnection.getInstance().getConnection().prepareStatement("SELECT * FROM doctor");
         ResultSet rst  = stm.executeQuery();
@@ -20,5 +43,75 @@ public class DoctorController {
             );
         }
         return ids;
+    }
+
+    public void btnadd(ActionEvent actionEvent) throws SQLException, ClassNotFoundException {
+        Doctor doctor = new Doctor(txtDid.getText(),Dname.getText(),txtaddress.getText(), (String) cmdEx.getValue());
+        Connection connection = DbConnection.getInstance().getConnection();
+        PreparedStatement preparedStatement = connection.prepareStatement("INSERT INTO doctor " + "VALUES(?,?,?,?)");
+        preparedStatement.setObject(1,doctor.getDID());
+        preparedStatement.setObject(2,doctor.getName());
+        preparedStatement.setObject(3,doctor.getAddress());
+        preparedStatement.setObject(4,doctor.getExperiennce());
+
+        int save = preparedStatement.executeUpdate();
+        if (save > 0) {
+            new Alert(Alert.AlertType.CONFIRMATION, "Saved", ButtonType.OK).show();
+        } else {
+            new Alert(Alert.AlertType.WARNING, "Try again", ButtonType.OK).show();
+        }
+
+    }
+
+    public void btnupdate(ActionEvent actionEvent) throws SQLException, ClassNotFoundException {
+        Doctor doctor = new Doctor(txtDid.getText(),Dname.getText(),txtaddress.getText(), (String) cmdEx.getValue());
+        Connection connection = DbConnection.getInstance().getConnection();
+        PreparedStatement preparedStatement = connection.prepareStatement("UPDATE doctor SET "+" Name=?,Address=?,Experiennce=? WHERE DID=?");
+        preparedStatement.setObject(1,doctor.getName());
+        preparedStatement.setObject(2,doctor.getAddress());
+        preparedStatement.setObject(3,doctor.getExperiennce());
+        preparedStatement.setObject(4,doctor.getDID());
+
+
+
+        int update=preparedStatement.executeUpdate();
+        if (update>0){
+            new Alert(Alert.AlertType.CONFIRMATION,"Updated",ButtonType.OK).show();
+        }else{
+            new Alert(Alert.AlertType.WARNING,"try again",ButtonType.OK).show();;
+        }
+
+    }
+
+    public void btnclear(ActionEvent actionEvent) {
+    }
+
+    public void btndelete(ActionEvent actionEvent) throws SQLException, ClassNotFoundException {
+        Connection connection = DbConnection.getInstance().getConnection();
+        PreparedStatement preparedStatement=connection.prepareStatement("DELETE FROM doctor WHERE DID=?");
+        preparedStatement.setObject(1,txtDid.getText());
+        int delete= preparedStatement.executeUpdate();
+        if (delete>0){
+            new Alert(Alert.AlertType.CONFIRMATION,"Deleted",ButtonType.OK).show();
+        }else {
+            new Alert(Alert.AlertType.WARNING,"Try again",ButtonType.OK).show();
+        }
+
+
+    }
+
+    public void btnsearch(ActionEvent actionEvent) throws SQLException, ClassNotFoundException {
+        Connection connection = DbConnection.getInstance().getConnection();
+        PreparedStatement preparedStatement= connection.prepareStatement("SELECT * FROM doctor WHERE DID=?");
+        preparedStatement.setObject(1,DID.getText());
+
+        ResultSet resultSet= preparedStatement.executeQuery();
+        while (resultSet.next()){
+            txtDid.setText(resultSet.getString(1));
+            Dname.setText(resultSet.getString(2));
+            txtaddress.setText(resultSet.getString(3));
+            cmdEx.setValue(resultSet.getString(4));
+
+        }
     }
 }
--- src/Controller/PaymentController.java
+++ src/Controller/PaymentController.java
@@ -3,6 +3,7 @@ package Controller;
 import com.jfoenix.controls.JFXComboBox;
 import com.jfoenix.controls.JFXTextField;
 import db.DbConnection;
+import javafx.collections.FXCollections;
 import javafx.event.ActionEvent;
 import javafx.scene.control.Alert;
 import javafx.scene.control.ButtonType;
@@ -11,6 +12,7 @@ import model.Payment;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 
 public class PaymentController {
@@ -21,10 +23,17 @@ public class PaymentController {
     public JFXComboBox cmdCusId;
     public TextField txtpaymentId;
 
+    public void initialize(){
+        loadCustomerIds();
+    }
+
+
+
+
     public void btnadd(ActionEvent actionEvent) throws SQLException, ClassNotFoundException {
         Payment payment = new Payment(txtpayment.getText(), (String) cmdCusId.getValue(),txtPname.getText(),txtbank.getText(),txtaccountno.getText());
         Connection connection = DbConnection.getInstance().getConnection();
-        PreparedStatement preparedStatement = connection.prepareStatement("INSERT INTO payment" + "VALUES(?,?,?,?,?)");
+        PreparedStatement preparedStatement = connection.prepareStatement("INSERT INTO payment" + " VALUES(?,?,?,?,?)");
         preparedStatement.setObject(1,payment.getPID());
         preparedStatement.setObject(2,payment.getCID());
         preparedStatement.setObject(3,payment.getProductname());
@@ -38,15 +47,62 @@ public class PaymentController {
         }
     }
 
-    public void btnupdate(ActionEvent actionEvent) {
+    public void btnupdate(ActionEvent actionEvent) throws SQLException, ClassNotFoundException {
+        Payment payment = new Payment(txtpayment.getText(), (String) cmdCusId.getValue(),txtPname.getText(),txtbank.getText(),txtaccountno.getText());
+        Connection connection = DbConnection.getInstance().getConnection();
+        PreparedStatement preparedStatement = connection.prepareStatement("UPDATE payment SET " + "CID=?,Productname=?,Bank=?,accountno=? WHERE PID=?");
+
+        preparedStatement.setObject(1,payment.getCID());
+        preparedStatement.setObject(2,payment.getProductname());
+        preparedStatement.setObject(3,payment.getBank());
+        preparedStatement.setObject(4,payment.getAccountno());
+        preparedStatement.setObject(5,payment.getPID());
+
+        int update=preparedStatement.executeUpdate();
+        if (update>0){
+            new Alert(Alert.AlertType.CONFIRMATION,"Updated",ButtonType.OK).show();
+        }else{
+            new Alert(Alert.AlertType.WARNING,"try again",ButtonType.OK).show();
+        }
+
     }
 
-    public void btnDelecte(ActionEvent actionEvent) {
+    public void btnDelecte(ActionEvent actionEvent) throws SQLException, ClassNotFoundException {
+        Connection connection = DbConnection.getInstance().getConnection();
+        PreparedStatement preparedStatement = connection.prepareStatement("DELETE FROM payment WHERE PID=?");
+        preparedStatement.setObject(1, txtpaymentId.getText());
+        int delete = preparedStatement.executeUpdate();
+        if (delete > 0) {
+            new Alert(Alert.AlertType.CONFIRMATION, "Deleted", ButtonType.OK).show();
+        } else {
+            new Alert(Alert.AlertType.WARNING, "Try again", ButtonType.OK).show();
+        }
     }
 
     public void btnClear(ActionEvent actionEvent) {
     }
 
-    public void btnsearch(ActionEvent actionEvent) {
+    public void btnsearch(ActionEvent actionEvent) throws SQLException, ClassNotFoundException {
+        Connection connection = DbConnection.getInstance().getConnection();
+        PreparedStatement preparedStatement = connection.prepareStatement("SELECT * FROM payment WHERE PID=?");
+preparedStatement.setObject(1,txtpaymentId.getText());
+        ResultSet resultSet= preparedStatement.executeQuery();
+        while (resultSet.next()){
+          txtpayment.setText(resultSet.getString(1));
+            cmdCusId.setValue(resultSet.getString(2));
+            txtPname.setText(resultSet.getString(3));
+            txtbank.setText(resultSet.getString(4));
+            txtaccountno.setText(resultSet.getString(5));
+
+
+        }
+
+    }
+    private void loadCustomerIds() {
+        try {
+            cmdCusId.setItems(FXCollections.observableArrayList(CustomerCrudController.getCustomerIds()));
+        } catch (SQLException | ClassNotFoundException throwables) {
+            throwables.printStackTrace();
+        }
     }
 }
