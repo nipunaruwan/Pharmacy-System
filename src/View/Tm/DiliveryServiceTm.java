d278c7be4eb9f501e536c04dd89a4aaf07b1c09b
mipunaruwan <nipunakalpa1234@gmail.com>
2022-11-06 10:35:40 +0530

    DiliverTm is encapsulation

--- src/View/Tm/DiliveryServiceTm.java
+++ src/View/Tm/DiliveryServiceTm.java
@@ -1,4 +1,39 @@
 package View.Tm;
 
 public class DiliveryServiceTm {
+    private String DSID;
+    private String DSNAME;
+
+    public DiliveryServiceTm() {
+    }
+
+    public DiliveryServiceTm(String DSID, String DSNAME) {
+        this.setDSID(DSID);
+        this.setDSNAME(DSNAME);
+    }
+
+    public String getDSID() {
+        return DSID;
+    }
+
+    public void setDSID(String DSID) {
+        this.DSID = DSID;
+    }
+
+    public String getDSNAME() {
+        return DSNAME;
+    }
+
+    public void setDSNAME(String DSNAME) {
+        this.DSNAME = DSNAME;
+    }
+
+    @Override
+    public String toString() {
+        return "DiliveryService{" +
+                "DSID='" + DSID + '\'' +
+                ", DSNAME='" + DSNAME + '\'' +
+                '}';
+    }
+
 }
