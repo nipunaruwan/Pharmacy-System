72c843b517a84622ad24b0e67f8276f0d87929fb
mipunaruwan <nipunakalpa1234@gmail.com>
2022-11-06 10:31:23 +0530

    admintm is encapsulation

--- src/View/Tm/AdminTm.java
+++ src/View/Tm/AdminTm.java
@@ -1,4 +1,50 @@
 package View.Tm;
 
 public class AdminTm {
+    private String ADMINID;
+    private String USERNAME;
+    private String PASSWORD;
+
+
+    public AdminTm() {
+    }
+
+    public AdminTm(String ADMINID, String USERNAME, String PASSWORD) {
+        this.setADMINID(ADMINID);
+        this.setUSERNAME(USERNAME);
+        this.setPASSWORD(PASSWORD);
+    }
+
+    public String getADMINID() {
+        return ADMINID;
+    }
+
+    public void setADMINID(String ADMINID) {
+        this.ADMINID = ADMINID;
+    }
+
+    public String getUSERNAME() {
+        return USERNAME;
+    }
+
+    public void setUSERNAME(String USERNAME) {
+        this.USERNAME = USERNAME;
+    }
+
+    public String getPASSWORD() {
+        return PASSWORD;
+    }
+
+    public void setPASSWORD(String PASSWORD) {
+        this.PASSWORD = PASSWORD;
+    }
+
+    @Override
+    public String toString() {
+        return "Admin{" +
+                "ADMINID='" + ADMINID + '\'' +
+                ", USERNAME='" + USERNAME + '\'' +
+                ", PASSWORD='" + PASSWORD + '\'' +
+                '}';
+    }
 }
