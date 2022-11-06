3ea2d404675cb30987ed60928e460bbb182fd582
mipunaruwan <nipunakalpa1234@gmail.com>
2022-11-06 10:37:38 +0530

    itemtm is encapsulation

--- src/View/Tm/DoctorTm.java
+++ src/View/Tm/DoctorTm.java
@@ -1,4 +1,60 @@
 package View.Tm;
 
 public class DoctorTm {
+    private String DID;
+    private String Name;
+    private String Address;
+    private String Experiennce;
+
+    public DoctorTm() {
+    }
+
+    public DoctorTm(String DID, String name, String address, String experiennce) {
+        this.setDID(DID);
+        setName(name);
+        setAddress(address);
+        setExperiennce(experiennce);
+    }
+
+    public String getDID() {
+        return DID;
+    }
+
+    public void setDID(String DID) {
+        this.DID = DID;
+    }
+
+    public String getName() {
+        return Name;
+    }
+
+    public void setName(String name) {
+        Name = name;
+    }
+
+    public String getAddress() {
+        return Address;
+    }
+
+    public void setAddress(String address) {
+        Address = address;
+    }
+
+    public String getExperiennce() {
+        return Experiennce;
+    }
+
+    public void setExperiennce(String experiennce) {
+        Experiennce = experiennce;
+    }
+
+    @Override
+    public String toString() {
+        return "Doctor{" +
+                "DID='" + DID + '\'' +
+                ", Name='" + Name + '\'' +
+                ", Address='" + Address + '\'' +
+                ", Experiennce='" + Experiennce + '\'' +
+                '}';
+    }
 }
