29ad3191267d8722b03ac145136632af39757cb8
mipunaruwan <nipunakalpa1234@gmail.com>
2022-11-06 10:49:52 +0530

    paymentTm is encapsulation

--- src/View/Tm/OrderTm.java
+++ src/View/Tm/OrderTm.java
@@ -1,4 +1,71 @@
 package View.Tm;
 
 public class OrderTm {
+    private String OID;
+    private String CID;
+    private String DsID;
+    private String DATE;
+    private String TIME;
+
+    public OrderTm() {
+    }
+
+    public OrderTm(String OID, String CID, String dsID, String DATE, String TIME) {
+        this.setOID(OID);
+        this.setCID(CID);
+        setDsID(dsID);
+        this.setDATE(DATE);
+        this.setTIME(TIME);
+    }
+
+    public String getOID() {
+        return OID;
+    }
+
+    public void setOID(String OID) {
+        this.OID = OID;
+    }
+
+    public String getCID() {
+        return CID;
+    }
+
+    public void setCID(String CID) {
+        this.CID = CID;
+    }
+
+    public String getDsID() {
+        return DsID;
+    }
+
+    public void setDsID(String dsID) {
+        DsID = dsID;
+    }
+
+    public String getDATE() {
+        return DATE;
+    }
+
+    public void setDATE(String DATE) {
+        this.DATE = DATE;
+    }
+
+    public String getTIME() {
+        return TIME;
+    }
+
+    public void setTIME(String TIME) {
+        this.TIME = TIME;
+    }
+
+    @Override
+    public String toString() {
+        return "Order{" +
+                "OID='" + OID + '\'' +
+                ", CID='" + CID + '\'' +
+                ", DsID='" + DsID + '\'' +
+                ", DATE='" + DATE + '\'' +
+                ", TIME='" + TIME + '\'' +
+                '}';
+    }
 }
--- src/View/Tm/OrderdetailsTm.java
+++ src/View/Tm/OrderdetailsTm.java
@@ -1,4 +1,71 @@
 package View.Tm;
 
 public class OrderdetailsTm {
+    private String  OID;
+    private String  PCODE;
+    private String CID;
+    private String DATE;
+    private String TIME;
+
+    public OrderdetailsTm() {
+    }
+
+    public OrderdetailsTm(String OID, String PCODE, String CID, String DATE, String TIME) {
+        this.setOID(OID);
+        this.setPCODE(PCODE);
+        this.setCID(CID);
+        this.setDATE(DATE);
+        this.setTIME(TIME);
+    }
+
+    public String getOID() {
+        return OID;
+    }
+
+    public void setOID(String OID) {
+        this.OID = OID;
+    }
+
+    public String getPCODE() {
+        return PCODE;
+    }
+
+    public void setPCODE(String PCODE) {
+        this.PCODE = PCODE;
+    }
+
+    public String getCID() {
+        return CID;
+    }
+
+    public void setCID(String CID) {
+        this.CID = CID;
+    }
+
+    public String getDATE() {
+        return DATE;
+    }
+
+    public void setDATE(String DATE) {
+        this.DATE = DATE;
+    }
+
+    public String getTIME() {
+        return TIME;
+    }
+
+    public void setTIME(String TIME) {
+        this.TIME = TIME;
+    }
+
+    @Override
+    public String toString() {
+        return "Orderdetails{" +
+                "OID='" + OID + '\'' +
+                ", PCODE='" + PCODE + '\'' +
+                ", CID='" + CID + '\'' +
+                ", DATE='" + DATE + '\'' +
+                ", TIME='" + TIME + '\'' +
+                '}';
+    }
 }
